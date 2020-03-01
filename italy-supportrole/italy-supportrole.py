a8a15ff0e816d1a03a1390513cb149b998c35d85
Pedro Arenas <nachito94@msn.com>
2020-03-01 17:18:59 -0300

    Update italy-supportrole.py

--- italy-supportrole/italy-supportrole.py
+++ italy-supportrole/italy-supportrole.py
@@ -20,10 +20,10 @@ class SupportRoleManagent(commands.Cog):
     async def givesupport(self, ctx: Context, user: discord.Member):
         role = get(user.guild.roles, id=683333884871573534)
         if role in user.roles:
-            await ctx.send(f"hey {ctx.author.name}, {user.name} already have the <@&{role.id}> role")
+            await ctx.send(f"Hey **{ctx.author.name}**, il ruolo <@&{role.id}> è già stato assegnato a **{user.name}**.")
         else:
             await user.add_roles(role)
-            await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving <@&{role.id}> role")
+            await ctx.send(f"Hey **{ctx.author.name}**, il ruolo <@&{role.id}> è stato assegnato correttamente a **{user.name}**.")
 
     @supportrole.command(name="remove")
     @commands.has_any_role(659513332218331155, 676408167063879715)
@@ -31,9 +31,9 @@ class SupportRoleManagent(commands.Cog):
         role = get(user.guild.roles, id=683333884871573534)
         if role in user.roles:
             await user.remove_roles(role)
-            await ctx.send(f"hey {ctx.author.name}, {user.name} has been remove <@&{role.id}> role")
+            await ctx.send(f"Hey **{ctx.author.name}**, il ruolo <@&{role.id}> è stato rimosso correttamente a **{user.name}**.")
         else:
-            await ctx.send(f"hey {ctx.author.name}, {user.name} not have the <@&{role.id}> role")
+            await ctx.send(f"Hey **{ctx.author.name}**, il ruolo <@&{role.id}> non è ancora stato assegnato a **{user.name}**.")
 
 def setup(bot):
     bot.add_cog(SupportRoleManagent(bot))
