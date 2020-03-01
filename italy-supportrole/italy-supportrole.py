87df4825fcc2dd1d2cb65c0a11ee13f19ac0c7a3
Pedro Arenas <nachito94@msn.com>
2020-03-01 16:56:55 -0300

    Update italy-supportrole.py

--- italy-supportrole/italy-supportrole.py
+++ italy-supportrole/italy-supportrole.py
@@ -10,21 +10,24 @@ class SupportRoleManagent(commands.Cog):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
 
-    @commands.command()
+    @group(name="supportrole", invoke_without_command=True)
+    @commands.has_any_role(659513332218331155, 676408167063879715)
+    async def autorole(self, ctx: Context) -> None:
+        await ctx.send_help(ctx.command)
+
+    @supportrole.command(name="give")
     @commands.has_any_role(659513332218331155, 676408167063879715)
     async def givesupport(self, ctx: Context, user: discord.Member):
         role = get(user.guild.roles, id=683333884871573534)
-        await ctx.send("its gone?")
         await user.add_roles(role)
-        await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving a role called: {role.name}")
+        await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving <@&{role.id}> role")
 
-    @commands.command()
+    @supportrole.command(name="remove")
     @commands.has_any_role(659513332218331155, 676408167063879715)
     async def removesupport(self, ctx: Context, user: discord.Member):
         role = get(user.guild.roles, id=683333884871573534)
-        await ctx.send("nah")
         await user.remove_roles(role)
-        await ctx.send(f"hey {ctx.author.name}, {user.name} has been remove of a role called: {role.name}")
+        await ctx.send(f"hey {ctx.author.name}, {user.name} has been remove <@&{role.id}> role")
 
 
 
