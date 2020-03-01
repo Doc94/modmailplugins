f874ce6d60fff2929cbc5bedf0dc0535f299435b
Pedro Arenas <nachito94@msn.com>
2020-03-01 16:43:22 -0300

    Update italy-supportrole.py

--- italy-supportrole/italy-supportrole.py
+++ italy-supportrole/italy-supportrole.py
@@ -13,6 +13,7 @@ class SupportRoleManagent(commands.Cog):
     @commands.has_any_role(659513332218331155, 676408167063879715)
     async def givesupport(ctx, user: discord.Member):
         role = get(user.guild.roles, id=683333884871573534)
+        await ctx.send("its gone?")
         await user.add_roles(role)
         await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving a role called: {role.name}")
 
@@ -20,6 +21,7 @@ class SupportRoleManagent(commands.Cog):
     @commands.has_any_role(659513332218331155, 676408167063879715)
     async def removesupport(ctx, user: discord.Member):
         role = get(user.guild.roles, id=683333884871573534)
+        await ctx.send("nah")
         await user.remove_roles(role)
         await ctx.send(f"hey {ctx.author.name}, {user.name} has been remove of a role called: {role.name}")
 
